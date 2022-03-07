c4ea6dbc44c9d615700baa212b25879dac4a5b28
unknown <yugmeshk2139@gmail.com>
2022-03-08 00:25:05 +0530

    Hoisting

---
+++ Hoisting/app.js
@@ -0,0 +1,11 @@
+getName();
+console.log(x);
+console.log(getName);
+var x=7;
+// function getName(){
+//     console.log("Namste JS")
+// }
+var getName =() =>{
+    console.log("Namste JS")
+}
+// console.log(getName)
